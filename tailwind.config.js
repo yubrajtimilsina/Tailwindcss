58cb1f237a38db647a6d27ec0074a3fcf6297dd5
Yubraj Timilsina <yubrajtimilsina32@gmail.com>
2024-08-29 16:14:47 +0545

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,23 @@
 module.exports = {
   content: ['*.{html,js}'],
   theme: {
-    extend: {},
-    fontFamily: {
-      domine :['Roboto:ital','serif']
-    }
+    extend: {
+      fontFamily: {
+        domine :['Roboto:ital','serif']
+      },
+      animation :{
+        'rotating' : 'rotating 5s linear infinite'
+      },
+      keyframes : {
+        rotating :{
+          '0%' : {transform : 'rotate(0.0deg)'},
+          '25%' : {transform : 'rotate(90deg)'},
+          '50%' : {transform : 'rotate(180deg)'},
+          '75%' : {transform : 'rotate(270deg)'},
+          '100%' : {transform : 'rotate(360deg)'},
+        }
+      }
+    },
   },
   plugins: [],
 }
